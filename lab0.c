5eb807bdae95d45bb151ed6bab4b47c0ccdfe811
layla-nassar <layla.nassar@uconn.edu>
2024-08-30 14:59:48 +0000

    lab0

--- lab0.c
+++ lab0.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
-int main(void)
-{
-printf("Hello, World!\n");
-return 0;
-int sum = 0;
-while i <= 200
+int main(void) {    
+    printf("Hello, World!\n");
 
+int sum = 0;
+int i = 2; 
+
+while (i < 200) {
+    sum += i;
+    i += 2;
+}
+
+printf("%d\n", sum);
+
+return 0;
 
 }
