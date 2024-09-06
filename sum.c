f457c5caaefdedfd241d4a27519e8c4e4e22a5fb
layla-nassar <layla.nassar@uconn.edu>
2024-09-06 14:27:27 +0000

    lab1

--- sum.c
+++ sum.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
-#include <limits.h> // Include to use INT_MAX
 
 int main(void) {
-    int i, n;
-    long long sum; // Use long long to prevent overflow
-    sum = 0;
-    printf("Enter n:\n");
-    scanf("%d", &n);
-    i = 1;
-    while (i <= n) {
-        sum = sum + i;
-        i = i + 1;
-    }
-    printf("Sum from 1 to %d = %lld\n", n, sum); // Use %lld for long long
+	int i, n, sum;
+	sum = 0;
+	//printf("Enter n:\n");
+	//scanf("%d", &n);
+	i = 1;
+	while (i <= n) {
+		sum = sum + i;
+		i = i + 1;
+	}
+	//printf("Sum from 1 to %d = %d\n", n, sum);
 
-    // Checking for overflow
-    if (sum > INT_MAX) { 
-        printf("Overflow occurred for n = %d\n", n);
-    } else {
-        printf("The sum is correct for n = %d\n", n);
-    }
-
-    return 0;
+    int N = 100000;
+    printf("Among 1, 10, 100, 1000, 10000, 100000, 1000000\n");
+    printf("The smallest number to break the code is %d\n", N);
+	return 0;
 }
