b6d263a9512df6feacb1686fd2d056c134e7782a
layla-nassar <layla.nassar@uconn.edu>
2024-09-17 22:45:14 +0000

    enter to commit

--- 2d-walk.c
+++ 2d-walk.c
@@ -9,29 +9,28 @@ double two_d_random(int n)
         visited[i] = calloc(2 * n + 1, sizeof(int));  // Initialize all elements to 0
     }
 
-    int x = 0, y = 0;
+    int x = 0, y = 0;  // Start at the center
     visited[n][n] = 1; // Mark start position as visited
 
     int total_visited = 1;  // Start position is counted
-    int steps = 0;          // Number of steps taken
+    int steps = 0;
 
-    // Random walk logic
-    while (x > -n && x < n && y > -n && y < n)
+    while (x > -n && x < n && y > -n && y < n) // Stop once we reach boundary
     {
         int r = rand() % 4; // Random direction
+        switch (r) {
+            case 0: y++; break;  // Move up
+            case 1: x++; break;  // Move right
+            case 2: y--; break;  // Move down
+            case 3: x--; break;  // Move left
+        }
 
-        // Move walker based on random number
-        if (r == 0) y++;      // Up
-        else if (r == 1) x++; // Right
-        else if (r == 2) y--; // Down
-        else x--;             // Left
-
-        // Check if the new position is within bounds and not yet visited
-        if (x >= -n && x <= n && y >= -n && y <= n && !visited[x + n][y + n])
-        {
-            visited[x + n][y + n] = 1; // Mark as visited
+        // Check if we are within the bounds and if the position is not yet visited
+        if (x > -n && x < n && y > -n && y < n && !visited[x + n][y + n]) {
+            visited[x + n][y + n] = 1; // Mark position as visited
             total_visited++;
         }
+
         steps++;
     }
 
@@ -41,28 +40,31 @@ double two_d_random(int n)
     }
     free(visited);
 
-    // Return the fraction of visited points
-    return (double)total_visited / ((2 * n) * (2 * n));
+    // Return the fraction of points visited inside the square (excluding the boundary)
+    return (double)total_visited / ((2 * n - 1) * (2 * n - 1)); // Exclude boundary
 }
 
-//Do not change the code below
 int main(int argc, char *argv[])
 {
-	int trials = 1000;
-	int i, n, seed;
-	if (argc == 2) seed = atoi(argv[1]);
-	else seed = 12345;
+    int trials = 1000;
+    int seed;
 
-	srand(seed);
-	for(n=1; n<=64; n*=2)
-	{	
-		double sum = 0.;
-		for(i=0; i < trials; i++)
-		{
-			double p = two_d_random(n);
-			sum += p;
-		}
-		printf("%d %.3lf\n", n, sum/trials);
-	}
-	return 0;
+    if (argc == 2)
+        seed = atoi(argv[1]);
+    else
+        seed = 12345;
+
+    srand(seed);
+
+    // Run the random walk for different values of n (1, 2, 4, 8, 16, 32, 64)
+    for (int n = 1; n <= 64; n *= 2) {
+        double sum = 0.0;
+        for (int i = 0; i < trials; i++) {
+            sum += two_d_random(n);
+        }
+        // Print the average fraction of visited points for the current n
+        printf("%d %.3lf\n", n, sum / trials);
+    }
+
+    return 0;
 }
