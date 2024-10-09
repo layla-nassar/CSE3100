f914d19c63407f7400864dd68e3c652ce88c6ef4
layla-nassar <layla.nassar@uconn.edu>
2024-10-09 20:15:21 +0000

    COMMIT TO MAIN

--- sum.c
+++ sum.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-
-int main(void) {
-	int i, n, sum;
-	sum = 0;
-	//printf("Enter n:\n");
-	//scanf("%d", &n);
-	i = 1;
-	while (i <= n) {
-		sum = sum + i;
-		i = i + 1;
+#include <stdlib.h>
+int main(int argc, char * argv[])
+{
+	if(argc != 2)
+	{
+		printf("Usage: %s n\n", argv[0]);
+		return -1;
 	}
-	//printf("Sum from 1 to %d = %d\n", n, sum);
-
-    int N = 100000;
-    printf("Among 1, 10, 100, 1000, 10000, 100000, 1000000\n");
-    printf("The smallest number to break the code is %d\n", N);
+	int n = atoi(argv[1]);
+	int sum = 0;
+	for(int i=1; i<=n; i++)
+	{
+		sum += i;
+	}
+	printf("sum = %d\n", sum);
 	return 0;
 }
